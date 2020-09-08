6dd6b012bcb07744c7df1f821fa319a22efafa45
razidare <Radu.Zidarescu@endava.com>
2020-09-08 12:49:07 +0300

    added ACR cleanup script

---
+++ dockerHelpers/removeOldACRDockerImages.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Deletes all images from all ACR registries older than a specific number of months
+.EXAMPLE
+    ./removeOldACRDockerImages.ps1 -olderThanMonths 6 -acrName myAcr
+#>
+
+param (
+    [Parameter(Mandatory=$true)][int]$olderThanMonths,
+    [Parameter(Mandatory=$true)][string][ValidateNotNullOrEmpty()]$acrName
+)
+
+$referenceDate = [datetime]::Now.AddMonths(-$olderThanMonths).tostring("yyyy-MM-dd")
+
+$allRepositories = az acr repository list -n $acrName -o json | ConvertFrom-Json
+
+foreach ($repository in $allRepositories) {
+    $allTags = az acr repository show-tags --repository $repository -n $acrName --detail | ConvertFrom-Json
+    foreach ($tag in $allTags) {
+        $imageName = "$repository`:$($tag.name)"
+        if($referenceDate -gt $tag.createdTime){
+            Write-Verbose "Deleting image $imageName" -vb
+            az acr repository delete -n $acrName --image $imageName --yes
+        }
+        remove-variable imageName
+    }
+}
